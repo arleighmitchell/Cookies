9cde21631a339b4ba86919956287c90a2b5c7736
Arleigh Mitchell <arleighmitchell@gmail.com>
2016-09-26 11:59:54 -0700

    Create cookies

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
 	console.log("ready");
 
+	$("#save").click(function() {
+		document.cookie = "sugar=" + form.sugar.value
+		document.cookie = "chocolate=" + form.chocolate.value
+		document.cookie = "lemon=" + form.lemon.value
+
+	});
+
+	$("#sugar")[0].value = $.cookie("sugar") || ""
+	$("#chocolate")[0].value = $.cookie("chocolate") || ""
+	$("#lemon")[0].value = $.cookie("lemon") || ""
 });
