0eb000f229ccd94d9b25ea110deeda7e34ecb52b
Arleigh Mitchell <arleighmitchell@gmail.com>
2016-09-27 10:31:25 -0700

    Add buttons to increase each input value by 1

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,4 +11,25 @@ $(document).ready(function() {
 	$("#sugar")[0].value = $.cookie("sugar") || ""
 	$("#chocolate")[0].value = $.cookie("chocolate") || ""
 	$("#lemon")[0].value = $.cookie("lemon") || ""
+
+	$("#addOneSugar").click(function() {
+		var sugar = $("#sugar");
+		sugar.val(parseInt(sugar.val()) + 1);
+	});
+
+	$("#addOneChocolate").click(function() {
+		var chocolate = $("#chocolate");
+		chocolate.val(parseInt(chocolate.val()) + 1);
+	});
+
+	$("#addOneLemon").click(function() {
+		var lemon = $("#lemon");
+		lemon.val(parseInt(lemon.val()) + 1);
+	});
 });
+/* enter correct repository in terminal documents/projects/cookies(master) 
+Then run this in terminal:
+echo -e '\nalias served="ruby -run -e httpd . -p 5000"' >> ~/.bash_profile && source ~/.bash_profile
+Then type 'served' in terminal
+Then enter visit this site in browser:
+http://localhost:5000/
